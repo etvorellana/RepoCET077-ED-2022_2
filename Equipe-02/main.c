64de9266eac8f3737e0dcf8baba64df6d04a2d33
mslisboa <mathewslysboa@gmail.com>
2022-10-23 19:19:35 -0300

    Equipe 2 coding - Código Revisado

--- Equipe-02/main.c
+++ Equipe-02/main.c
@@ -3,25 +3,32 @@
 #include <stdlib.h>
 
 int main(void) {
-  srand(time(NULL));
   TListAlunos *listaA, *listaB, *listaC;
+  srand(time(NULL));
+
   listaA = initListaAluno(LSIZE, FALSE);
   listaB = initListaAluno(LSIZE, TRUE);
   listaC = initListaAluno(listaA->cap, listaA->eOrd);
+  
   gerarAluno(listaA, 4);
   gerarAluno(listaB, 4);
+  
   printf("\n\nLISTA A DESORDENADA\n\n");
   ExibirAluno(listaA);
+  
   printf("\n\nLISTA B ORDENADA\n\n");
   ExibirAluno(listaB);
+  
   HeapSort(listaA, listaA->aluno, listaA->tam);
   printf("\n\nLISTA A ORDENADA\n\n");
   ExibirAluno(listaA);
+  
   if (incAluno(listaB->aluno[0], listaA)) {
     printf("Aluno adicionado\n");
   } else {
     printf("Aluno não adiconado, lista cheia\n");
   }
+  
   printf("\n\nLISTA A COM B[0] ORDENADO\n\n");
   ExibirAluno(listaA);
 
@@ -30,6 +37,7 @@ int main(void) {
   } else {
     printf("Aluno não removido, lista vazia ou aluno não esta na lista\n");
   }
+  
   printf("\n\nLISTA B COM B[0] EXCLUIDO\n\n");
   ExibirAluno(listaB);
 
@@ -38,7 +46,7 @@ int main(void) {
     printf("Aluno não encontrado!\n");
   } else {
     printf("\n\nALUNO B[0] PROCURADO NA LISTA A\n\n%s\n",
-           listaA->aluno[pos].email);
+    listaA->aluno[pos].email);
   }
   
   interListas(listaA, listaB, listaC);
