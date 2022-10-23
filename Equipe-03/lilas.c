c07ce0b11e6cecd1aadb18632cbaa8a642ff0830
Bruno Schumacher Farias Souza <brunoschumacherfsouza@gmail.com>
2022-10-23 20:58:37 -0300

    equipe 3

--- Equipe-03/lilas.c
+++ Equipe-03/lilas.c
@@ -1,109 +1,200 @@
+#include "lilas.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "lilas.h"
 
-TListAlunos* initListaAluno(int cap, int eOrd)
-{
-    TListAlunos *lista;
-    lista = (TListAlunos*) malloc(sizeof(TListAlunos));
-    lista->tam = 0;
-    lista->cap = cap;
-    lista->eOrd = eOrd;
-    if(eOrd)
-    {
-        lista->aluno = (TAluno*) malloc(cap*sizeof(TAluno));
-    }else{
-        lista->aluno = (TAluno*) malloc((cap+1)*sizeof(TAluno));
-    }
-    return lista;
-}
-
-int buscaAluno(int nm, TListAlunos *lista){
-    int i = 0;
-
-    for(i = 0; i < lista->tam; i++){
-        if(lista->aluno[i].numMatricula == nm){
-            return i;
-        }
-    }
-    return -1;
-}
-
-int incAluno(TAluno aluno, TListAlunos *lista){
-    int tam = lista->tam;
-    if (lista->cap < tam + 1){
-        printf("Lista cheia\n!!!!");
-        return 0;
-    }
-    lista->aluno[tam + 1] = aluno;
-    if(lista->eOrd){
-        ordenarLista(lista);
-    }
-    return 1;
+TListAlunos *initListaAluno(int cap, int eOrd) {
+  TListAlunos *lista;
+  
+  lista = (TListAlunos *)malloc(sizeof(TListAlunos));
+  
+  lista->tam = 0;
+  lista->cap = cap;
+  lista->eOrd = eOrd;
+  
+  if (eOrd) {
+    lista->aluno = (TAluno *)malloc(cap * sizeof(TAluno));
+  } else {
+    lista->aluno = (TAluno *)malloc((cap + 1) * sizeof(TAluno));
+  }
+  
+  return lista;
 }
 
 
-int remAluno(TAluno *aluno, TListAlunos *lista){
-    if(lista->tam == 0){
-        printf("Lista Vazia");
-        return 0;
-    }
-    aluno = &lista->aluno[lista->tam + 1];
-    lista->tam--;
-    return 1;
-}
-
-int buscaPorNome(char nome[], TListAlunos *lista){
-    int i = 0;
-
-    for(i = 0; i < lista->tam; i++){
-        if(!strcmp(nome, lista->aluno[i].nome)){
-            return i;
-        }
-    }
-    return -1;
-
-}
-
-int buscaPorEMail(char email[], TListAlunos *lista){
-    int i = 0;
-
-    for(i = 0; i < lista->tam; i++){
-        if(!strcmp(email, lista->aluno[i].email)){
-            return i;
-        }
-    }
-    return -1;
+int buscaAluno(int nm, TListAlunos *lista) {
+  if (lista->eOrd) {
+    return buscaBin(nm, lista);
+  } else {
+    return buscaSeq(nm, lista);
+  }
 }
 
 
-TListAlunos* uniaoListas(TListAlunos *listaA, TListAlunos *listaB){
-    TListAlunos *ListaUnida;
-    int tam = listaA->tam + listaB->tam, i, j = 0;
-    ListaUnida = malloc(tam * sizeof(TListAlunos));
-    for(i = 0; i < listaA->tam; i++){
-        ListaUnida->aluno[i] = listaA->aluno[i];
-    }
-    for(i = 0; i < tam; i++, j++){
-        ListaUnida->aluno[i] = listaB->aluno[j];
-    }
-    ListaUnida->tam = tam;
-    ListaUnida->cap = listaA->cap + listaB->cap;
-    return ListaUnida;
-}
-
-
-void ordenarLista(TListAlunos *lista){
-  int i, j;
-  TAluno aluno;
-  for(i = 0; i < lista->tam; i++){
-    for(j = 0; j < lista->tam; j++){
-      if(lista->aluno[j].numMatricula > lista->aluno[j + 1].numMatricula){
-        aluno = lista->aluno[j];
-        lista->aluno[j] = lista->aluno[j + 1];
-        lista->aluno[j + 1] = aluno;
-      }
+int buscaSeq(int matricula, TListAlunos *lista) {
+  for (int i = 0; i < lista->tam; i++) {
+    if (lista->aluno[i].numMatricula == matricula) {
+      return i;
     }
   }
+
+  return lista->tam;
+}
+
+
+int buscaAlunoEmail(char *email, TListAlunos *lista) {
+  for (int i = 0; i < lista->tam; i++) {
+    if (!(strcmp(email, lista->aluno[i].email))) {
+      return i;
+    } 
+  }
+  
+  return lista->tam;
+}
+
+
+int buscaBin(int matricula, TListAlunos *lista) {
+  int inicio = 0;
+  int fim = lista->tam - 1;
+  int i;
+
+  while (inicio <= fim) {
+    i = (inicio + fim) / 2;
+    
+    if (lista->aluno[i].numMatricula == matricula) {
+      return i;
+    }
+
+    if (lista->aluno[i].numMatricula < matricula) {
+      inicio = i + 1;
+    } else {
+      fim = i;
+    }
+  }
+
+  
+  return i;
+}
+
+
+
+int incAluno(TAluno registro, TListAlunos *Tlista) {
+  int posicao = buscaAluno(registro.numMatricula, Tlista), tam = Tlista->tam;
+
+
+  if(Tlista->tam == Tlista->cap) return 0;
+  else {
+
+    if(Tlista->eOrd){
+      if(Tlista->aluno[posicao].numMatricula == registro.numMatricula) {
+        return 0;
+      }else {
+        while ((tam-1) >= posicao){
+          Tlista->aluno[tam] = Tlista->aluno[tam-1];
+          tam--;
+        }
+        Tlista->aluno[posicao] = registro;
+        Tlista->tam++;
+      }
+    } else {
+      if(posicao != Tlista->tam) {
+        return 0;
+      }else {
+        Tlista->aluno[posicao] = registro;
+        Tlista->tam++;
+      }
+    }
+
+    return 1;
+  }
+}
+
+
+int remAluno(TAluno *Taluno, TListAlunos *Tlista) {
+  if (Tlista->tam == 0) {
+    return FALSE;
+  }
+
+  int posicao, j;
+
+  posicao = buscaAluno(Taluno->numMatricula, Tlista);
+
+  if (Tlista->eOrd) {
+    if (Tlista->aluno[posicao].numMatricula == Taluno->numMatricula) {
+      for (j = posicao; j < (Tlista->tam - 1); j++) Tlista->aluno[j] = Tlista->aluno[j + 1];
+        
+      Tlista->tam--;
+        
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  } else {
+    if (posicao < Tlista->tam) {
+      Tlista->aluno[posicao] = Tlista->aluno[Tlista->tam-1];
+      Tlista->tam--;
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+}
+
+void interListas(TListAlunos *listaA, TListAlunos *listaB, TListAlunos *listaC) {
+
+  for (int i = 0; i < listaA->tam; i++) {
+    int pos = buscaSeq(listaA->aluno[i].numMatricula, listaB);
+      
+    if (pos < listaB->tam) {
+      listaC->aluno[listaC->tam] = listaA->aluno[i];
+      listaC->tam++;
+    }
+  }
+}
+
+
+
+void HeapSort(TListAlunos *infolistas, TAluno *lista, int tam) {
+  TAluno aluno;
+  int i;
+  
+  for (i = (tam - 1) / 2; i >= 0;
+       i--) {                
+    criaheap(lista, i, tam); 
+  }
+  
+  for (i = tam - 1; i >= 1; i--) {
+    aluno = lista[0];
+    lista[0] = lista[i];
+    lista[i] = aluno; 
+    criaheap(lista, 0, i - 1);
+  }
+  
+  infolistas->eOrd = TRUE;
+}
+
+
+void criaheap(TAluno *lista, int i, int f) { 
+  TAluno aluno;
+  
+  aluno = lista[i]; 
+  int j = i * 2 + 1;
+  
+  while (j <= f) { 
+    if (j < f) {
+      if (lista[j].numMatricula <
+          lista[j + 1].numMatricula) { 
+        j = j + 1;                    
+      }     
+    }
+    
+    if (aluno.numMatricula < lista[j].numMatricula) {
+      lista[i] = lista[j];
+      i = j;
+      j = 2 * i + 1;
+    } else {
+      j = f + 1;
+    }
+  }
+  
+  lista[i] = aluno;
 }
