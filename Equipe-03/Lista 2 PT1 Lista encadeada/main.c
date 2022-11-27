10bc2083649662b62f4389dfc493a42414bb5509
Bruno Schumacher Farias Souza <brunoschumacherfsouza@gmail.com>
2022-11-26 21:47:43 -0300

    fix equipe 3. Esqueci de atualizar

--- Equipe-03/Lista 2 PT1 Lista encadeada/main.c
+++ Equipe-03/Lista 2 PT1 Lista encadeada/main.c
@@ -10,148 +10,162 @@ typedef struct TAluno{
   
 } TAluno;
 
-void empilha(TAluno**, char[], int, char[]);
-void desempilha(TAluno**);
-void imprime_pilha(TAluno*);
-void enfileira(TAluno**, char[], int, char[]);
-void desenfileira(TAluno**);
-void imprime_fila(TAluno*);
+typedef struct Tfila{
+  TAluno *inicio;
+  TAluno *fim;
+  int tam;
+} Tfila;
+
+typedef struct Tpilha{
+  TAluno *topo;
+  int tam;
+} Tpilha;
+
+void empilhar(TAluno *aluno, Tpilha *pilha);
+void desempilhar(TAluno *aluno, Tpilha *pilha);
+void enfileirar(TAluno *aluno, Tfila *fila);
+void desenfileirar(TAluno *aluno, Tfila *fila);
+void imprime_pilha(Tpilha *pilha);
+void imprime_fila(Tfila *fila);
 void menu();
 
 int main(){
-    TAluno *pilha = NULL;
-    TAluno *fila = NULL;
-    int opcao;
-    char nome[100];
-    int matricula;
-    char email[100];
-    do{
-        menu();
-        scanf("%d", &opcao);
-        switch(opcao){
-            case 1:
-                printf("Digite o nome: ");
-                scanf("%s", nome);
-                printf("Digite a matricula: ");
-                scanf("%d", &matricula);
-                printf("Digite o email: ");
-                scanf("%s", email);
-                empilha(&pilha, nome, matricula, email);
-                break;
-            case 2:
-                desempilha(&pilha);
-                break;
-            case 3:
-                imprime_pilha(pilha);
-                break;
-            case 4:
-                printf("Digite o nome: ");
-                scanf("%s", nome);
-                printf("Digite a matricula: ");
-                scanf("%d", &matricula);
-                printf("Digite o email: ");
-                scanf("%s", email);
-                enfileira(&fila, nome, matricula, email);
-                break;
-            case 5:
-                desenfileira(&fila);
-                break;
-            case 6:
-                imprime_fila(fila);
-                break;
-            case 7:
-                printf("Saindo...\n");
-                break;
-            default:
-                printf("Opcao invalida!\n");
-        }
-    }while(opcao != 7);
+    menu();
     return 0;
 }
 
-void empilha(TAluno **pilha, char nome[], int matricula, char email[]){
-    TAluno *novo = (TAluno*)malloc(sizeof(TAluno));
-    strcpy(novo->nome, nome);
-    novo->matricula = matricula;
-    strcpy(novo->email, email);
-    novo->prox = *pilha;
-    *pilha = novo;
+
+
+void empilhar(TAluno *aluno, Tpilha *pilha){
+    TAluno *novo = (TAluno*) malloc(sizeof(TAluno));
+    novo->matricula = aluno->matricula;
+    strcpy(novo->nome, aluno->nome);
+    strcpy(novo->email, aluno->email);
+    novo->prox = pilha->topo;
+    pilha->topo = novo;
+    pilha->tam++;
 }
 
-void desempilha(TAluno **pilha){
-    if(*pilha == NULL){
+void desempilhar(TAluno *aluno, Tpilha *pilha){
+    if (pilha->tam == 0){
         printf("Pilha vazia!\n");
-    }else{
-        TAluno *aux = *pilha;
-        *pilha = (*pilha)->prox;
-        free(aux);
+        return;
     }
+    TAluno *aux = pilha->topo;
+    pilha->topo = aux->prox;
+    aluno->matricula = aux->matricula;
+    strcpy(aluno->nome, aux->nome);
+    strcpy(aluno->email, aux->email);
+    free(aux);
+    pilha->tam--;
 }
 
-void imprime_pilha(TAluno *pilha){
-    if(pilha == NULL){
-        printf("Pilha vazia!\n");
-    }else{
-        TAluno *aux = pilha;
-        while(aux != NULL){
-            printf("Nome: %s\n", aux->nome);
-            printf("Matricula: %d\n", aux->matricula);
-            printf("Email: %s\n", aux->email);
-            aux = aux->prox;
-        }
-    }
-}
-
-void enfileira(TAluno **fila, char nome[], int matricula, char email[]){
-    TAluno *novo = (TAluno*)malloc(sizeof(TAluno));
-    strcpy(novo->nome, nome);
-    novo->matricula = matricula;
-    strcpy(novo->email, email);
+void enfileirar(TAluno *aluno, Tfila *fila){
+    TAluno *novo = (TAluno*) malloc(sizeof(TAluno));
+    novo->matricula = aluno->matricula;
+    strcpy(novo->nome, aluno->nome);
+    strcpy(novo->email, aluno->email);
     novo->prox = NULL;
-    if(*fila == NULL){
-        *fila = novo;
+    if(fila->fim == NULL){
+        fila->inicio = novo;
     }else{
-        TAluno *aux = *fila;
-        while(aux->prox != NULL){
-            aux = aux->prox;
-        }
-        aux->prox = novo;
+        fila->fim->prox = novo;
     }
+    fila->fim = novo;
+    fila->tam++;
 }
 
-void desenfileira(TAluno **fila){
-    if(*fila == NULL){
+void desenfileirar(TAluno *aluno, Tfila *fila){
+    if (fila->tam == 0){
         printf("Fila vazia!\n");
-    }else{
-        TAluno *aux = *fila;
-        *fila = (*fila)->prox;
-        free(aux);
+        return;
+    }
+    TAluno *aux = fila->inicio;
+    fila->inicio = aux->prox;
+    aluno->matricula = aux->matricula;
+    strcpy(aluno->nome, aux->nome);
+    strcpy(aluno->email, aux->email);
+    free(aux);
+    fila->tam--;
+}
+
+void imprime_pilha(Tpilha *pilha){
+    TAluno *aux = pilha->topo;
+    while(aux != NULL){
+        printf("Matricula: %d\n", aux->matricula);
+        printf("Nome: %s\n", aux->nome);
+        printf("Email: %s\n", aux->email);
+        aux = aux->prox;
     }
 }
 
-void imprime_fila(TAluno *fila){
-    if(fila == NULL){
-        printf("Fila vazia!\n");
-    }else{
-        TAluno *aux = fila;
-        while(aux != NULL){
-            printf("Nome: %s\n", aux->nome);
-            printf("Matricula: %d\n", aux->matricula);
-            printf("Email: %s\n", aux->email);
-            aux = aux->prox;
-        }
+void imprime_fila(Tfila *fila){
+    TAluno *aux = fila->inicio;
+    while(aux != NULL){
+        printf("Matricula: %d\n", aux->matricula);
+        printf("Nome: %s\n", aux->nome);
+        printf("Email: %s\n", aux->email);
+        aux = aux->prox;
     }
 }
-
-
 
 void menu(){
-    printf("1 - Empilhar\n");
-    printf("2 - Desempilhar\n");
-    printf("3 - Imprimir pilha\n");
-    printf("4 - Enfileirar\n");
-    printf("5 - Desenfileirar\n");
-    printf("6 - Imprimir fila\n");
-    printf("7 - Sair\n");
-    printf("Digite a opcao: ");
+    int opcao;
+    Tpilha pilha;
+    Tfila fila;
+    TAluno aluno;
+    pilha.topo = NULL;
+    pilha.tam = 0;
+    fila.inicio = NULL;
+    fila.fim = NULL;
+    fila.tam = 0;
+    do{
+        printf("1 - Empilhar\n");
+        printf("2 - Desempilhar\n");
+        printf("3 - Enfileirar\n");
+        printf("4 - Desenfileirar\n");
+        printf("5 - Imprimir pilha\n");
+        printf("6 - Imprimir fila\n");
+        printf("0 - Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+        switch(opcao){
+            case 1:
+                printf("Matricula: ");
+                scanf("%d", &aluno.matricula);
+                printf("Nome: ");
+                scanf("%s", aluno.nome);
+                printf("Email: ");
+                scanf("%s", aluno.email);
+                empilhar(&aluno, &pilha);
+                break;
+            case 2:
+                desempilhar(&aluno, &pilha);
+                break;
+            case 3:
+                printf("Matricula: ");
+                scanf("%d", &aluno.matricula);
+                printf("Nome: ");
+                scanf("%s", aluno.nome);
+                printf("Email: ");
+                scanf("%s", aluno.email);
+                enfileirar(&aluno, &fila);
+                break;
+            case 4:
+                desenfileirar(&aluno, &fila);
+                break;
+            case 5:
+                imprime_pilha(&pilha);
+                break;
+            case 6:
+                imprime_fila(&fila);
+                break;
+            case 0:
+                break;
+            default:
+                printf("Opção inválida\n");
+                break;
+        }
+    }while(opcao != 0);
 }
+
