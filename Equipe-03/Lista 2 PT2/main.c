887e08b1c147aae0022489e76210e029330e90d3
Bruno Schumacher Farias Souza <brunoschumacherfsouza@gmail.com>
2022-12-01 00:28:10 -0300

    fix coding

--- Equipe-03/Lista 2 PT2/main.c
+++ Equipe-03/Lista 2 PT2/main.c
@@ -29,10 +29,12 @@ void preencher(TAluno *aluno, TNo *no);
 void empilhar_no(TNo *no, Pilha *pilha);
 void enfileirar_pilha(Pilha *pilha, Fila *fila);
 void desifileirar(Fila *fila, TNo *no);
+void preencher_no(TNo *no, TAluno *aluno);
 void imprime_fila(Fila *fila);
 void imprime_pilha(Pilha *pilha);
 void imprime_no(TNo *no);
-TNo buscar_aluno(TNo *no, int matricula);
+void empilhar(TAluno *aluno, Pilha *pilha);
+TNo* buscar_aluno(TNo *no, int matricula);
 void menu();
 
 int main(){
@@ -42,73 +44,83 @@ int main(){
 
 
 void preencher(TAluno *aluno, TNo *no){
-    TNo taluno = buscar_aluno(no, aluno->matricula);
-
-    if (taluno.aluno->matricula == aluno->matricula){
-        printf("Aluno ja cadastrado!\n");
+    TNo *taluno = buscar_aluno(no, aluno->matricula);
+    if(taluno != NULL){
+        printf("Aluno já cadastrado!\n");
         return;
     }
-    taluno.aluno->matricula = aluno->matricula;
-    strcpy(taluno.aluno->nome, aluno->nome);
-    strcpy(taluno.aluno->email, aluno->email);
-    printf("Aluno cadastrado com sucesso!\n");
-    printf("%d", taluno.aluno->matricula);
-    taluno.prox = NULL;
+    TNo *novo = (TNo*) malloc(sizeof(TNo));
+    novo->aluno = (TAluno*) malloc(sizeof(TAluno));
+    novo->aluno->matricula = aluno->matricula;
+    strcpy(novo->aluno->nome, aluno->nome);
+    strcpy(novo->aluno->email, aluno->email);
+    novo->prox = no->prox;
+    novo->ant = no;
+    no->prox = novo;
+    if(novo->prox != NULL){
+        novo->prox->ant = novo;
+    }
 }
 
 
 void empilha_no(TNo *no, Pilha *pilha){
     while(no->prox != NULL){
-        TAluno *novo = (TAluno*) malloc(sizeof(TAluno));
-        novo->matricula = no->aluno->matricula;
-        strcpy(novo->nome, no->aluno->nome);
-        strcpy(novo->email, no->aluno->email);
-        novo->prox = pilha->topo;
-        pilha->topo = novo;
-        pilha->tam++;
+        TAluno *aluno = (TAluno*) malloc(sizeof(TAluno));
+        aluno->matricula = no->prox->aluno->matricula;
+        strcpy(aluno->nome, no->prox->aluno->nome);
+        strcpy(aluno->email, no->prox->aluno->email);
+        empilhar(aluno, pilha);
         no = no->prox;
-        free(no->ant);
     }
+}
+
+void empilhar(TAluno *aluno, Pilha *pilha){
+    TAluno *novo = (TAluno*) malloc(sizeof(TAluno));
+    novo->matricula = aluno->matricula;
+    strcpy(novo->nome, aluno->nome);
+    strcpy(novo->email, aluno->email);
+    novo->prox = pilha->topo;
+    pilha->topo = novo;
+    pilha->tam++;
 }
 
 void enfileirar_pilha(Pilha *pilha, Fila *fila){
-    while(pilha->tam != 0){
+    while(pilha->topo != NULL){
+        TAluno *aluno = (TAluno*) malloc(sizeof(TAluno));
+        aluno->matricula = pilha->topo->matricula;
+        strcpy(aluno->nome, pilha->topo->nome);
+        strcpy(aluno->email, pilha->topo->email);
         TAluno *novo = (TAluno*) malloc(sizeof(TAluno));
-        novo->matricula = pilha->topo->matricula;
-        strcpy(novo->nome, pilha->topo->nome);
-        strcpy(novo->email, pilha->topo->email);
-        novo->prox = NULL;
-        if (fila->inicio == NULL){
+        novo->matricula = aluno->matricula;
+        strcpy(novo->nome, aluno->nome);
+        strcpy(novo->email, aluno->email);
+        if(fila->inicio == NULL){
             fila->inicio = novo;
             fila->fim = novo;
+            fila->tam++;
         }else{
             fila->fim->prox = novo;
             fila->fim = novo;
+            fila->tam++;
         }
-        fila->tam++;
         pilha->topo = pilha->topo->prox;
         pilha->tam--;
     }
-    free(pilha);
 }
 
 void desifileirar(Fila *fila, TNo *no){
-    TNo *aux;
-    while(fila->tam != 0){
-        TNo *novo = (TNo*) malloc(sizeof(TNo));
-        novo->aluno->matricula = fila->inicio->matricula;
-        strcpy(novo->aluno->nome, fila->inicio->nome);
-        strcpy(novo->aluno->email, fila->inicio->email);
-        novo->prox = no->prox;
-        no->prox = novo;
-        aux = no;
-        no = no->prox;
-        no->ant = aux;
+    while(fila->inicio != NULL){
+        TAluno *aluno = (TAluno*) malloc(sizeof(TAluno));
+        aluno->matricula = fila->inicio->matricula;
+        strcpy(aluno->nome, fila->inicio->nome);
+        strcpy(aluno->email, fila->inicio->email);
+        preencher(aluno, no);
         fila->inicio = fila->inicio->prox;
         fila->tam--;
     }
-    free(fila);
 }
+
+
 
 void imprime_fila(Fila *fila){
     TAluno *aux = fila->inicio;
@@ -130,7 +142,6 @@ void imprime_pilha(Pilha *pilha){
     }
 }
 
-
 void imprime_no(TNo *no){
     TNo *aux = no->prox;
     while(aux != NULL){
@@ -142,21 +153,15 @@ void imprime_no(TNo *no){
 }
 
 
-TNo buscar_aluno(TNo *no, int matricula){
-    TNo *aux = no, *aux2;
-    while(aux->aluno != NULL){
-        if (aux->aluno->matricula == matricula){
-            return *aux;
+TNo* buscar_aluno(TNo *no, int matricula){
+    TNo *aux = no->prox;
+    while(aux != NULL){
+        if(aux->aluno->matricula == matricula){
+            return aux;
         }
-        aux2 = aux;
         aux = aux->prox;
     }
-    aux = (TNo*) malloc(sizeof(TNo));
-    aux->aluno = (TAluno*) malloc(sizeof(TAluno));
-    aux->prox = NULL;
-    aux->ant = aux2;
-    aux2->prox = aux;
-    return *aux;
+    return aux;
 }
 
 
